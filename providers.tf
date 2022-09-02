19bf1c8f1d48a1b0158891c7bc221edd0663a334
manasianu <111309945+manasianu@users.noreply.github.com>
2022-09-03 02:55:16 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
   features {}
   
   subscription_id = "2134d62e-5ae7-49a1-9bd5-c793f3b0a74c"
-  client_id       = "eeea62ec-cc64-4e2a-867e-76554e57a839"
-  client_secret   = "xmv8Q~X-AhW5ilIE.y9Xt9aWGqmkiM1ogDzClaLK"
+  client_id       = "8f327716-a18e-4e60-bff0-fc96f0a20d29"
+  client_secret   = "nOp8Q~nWfSqswI_I5qHC46gxVhkAl7Qkae-NEcrW"
   tenant_id       = "33bc5ab6-4632-439e-8d43-201b74aaa8b3"
 }
